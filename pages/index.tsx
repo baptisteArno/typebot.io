68dd491eca6f812c60383c49ae2ad376513d4264
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2021-11-19 10:39:33 +0100

    Set node-linker to node_modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+import React from 'react'
+
+const HomePage = () => {
+  return <div>Welcome to "Next.js"!</div>
 }
 
-export default HomePage;
+export default HomePage
