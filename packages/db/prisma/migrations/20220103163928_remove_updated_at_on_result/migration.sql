77b553acf5b1ba1d121620a9f8ad140cc611f9d5
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-01-06 09:57:55 +0100

    revert(db): ⏪️ Remove migration files and push until db is stable

--- packages/db/prisma/migrations/20220103163928_remove_updated_at_on_result/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updatedAt` on the `Result` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Result" DROP COLUMN "updatedAt";
