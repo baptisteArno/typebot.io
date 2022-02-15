a8a03fbcf2f51e13960fa7ca4cae7b10b2cbe2da
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-02-15 09:48:22 +0100

    build: 👷 Remove Sentry traceSampling

--- apps/builder/sentry.client.config.js
+++ apps/builder/sentry.client.config.js
@@ -6,10 +6,4 @@ import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 })
