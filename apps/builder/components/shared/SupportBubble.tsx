341cd15e0bd9e963c9e79f683700b52d7456706b
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-02-19 11:02:41 +0100

    fix: 🐛 Typebot support bubble hidden vars

--- apps/builder/components/shared/SupportBubble.tsx
+++ apps/builder/components/shared/SupportBubble.tsx
@@ -1,13 +1,11 @@
 import { useTypebot } from 'contexts/TypebotContext'
 import { useUser } from 'contexts/UserContext'
-import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import { initBubble } from 'typebot-js'
 
 export const SupportBubble = () => {
   const { typebot } = useTypebot()
   const { user } = useUser()
-  const router = useRouter()
 
   useEffect(() => {
     initBubble({
@@ -16,14 +14,14 @@ export const SupportBubble = () => {
       backgroundColor: '#ffffff',
       button: { color: '#0042DA' },
       hiddenVariables: {
-        'Current URL': window.location.href,
         'User ID': user?.id,
+        Name: user?.name ?? undefined,
         Email: user?.email ?? undefined,
         'Typebot ID': typebot?.id,
       },
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router])
+  }, [user, typebot])
 
   return <></>
 }
