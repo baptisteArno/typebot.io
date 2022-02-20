65c206e00ab87935fdd8ce278c7f674cf7a6bd74
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-02-20 11:21:04 +0100

    fix(engine): 🐛 Parse condition value variable

--- packages/bot-engine/src/services/logic.ts
+++ packages/bot-engine/src/services/logic.ts
@@ -62,7 +62,7 @@ const executeComparison =
     if (!comparison?.variableId) return false
     const inputValue =
       variables.find((v) => v.id === comparison.variableId)?.value ?? ''
-    const { value } = comparison
+    const value = parseVariables(variables)(comparison.value)
     if (isNotDefined(value)) return false
     switch (comparison.comparisonOperator) {
       case ComparisonOperators.CONTAINS: {
