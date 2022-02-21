5edd63c5f6a33e8a3b1e13828a27697f403d3637
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-02-21 13:52:45 +0100

    feat(api): ✨ Add email to `me` endpoint

--- apps/viewer/pages/api/users/me.ts
+++ apps/viewer/pages/api/users/me.ts
@@ -8,7 +8,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const user = await authenticateUser(req)
     if (isNotDefined(user))
       return res.status(404).send({ message: 'User not found' })
-    return res.send({ id: user.id })
+    return res.send({ id: user.id, email: user.email })
   }
   return methodNotAllowed(res)
 }
