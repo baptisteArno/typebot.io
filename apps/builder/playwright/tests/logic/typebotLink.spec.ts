f6b518989c8eabd444c295942161074c5ac690fb
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-04-04 10:45:41 +0200

    ci: 💚 Improve linked typebot test robustness

--- apps/builder/playwright/tests/logic/typebotLink.spec.ts
+++ apps/builder/playwright/tests/logic/typebotLink.spec.ts
@@ -9,26 +9,26 @@ test('should be configurable', async ({ page }) => {
   const linkedTypebotId = cuid()
   await importTypebotInDatabase(
     path.join(__dirname, '../../fixtures/typebots/logic/linkTypebots/1.json'),
-    { id: typebotId }
+    { id: typebotId, name: 'My link typebot 1' }
   )
   await importTypebotInDatabase(
     path.join(__dirname, '../../fixtures/typebots/logic/linkTypebots/2.json'),
-    { id: linkedTypebotId }
+    { id: linkedTypebotId, name: 'My link typebot 2' }
   )
 
   await page.goto(`/typebots/${typebotId}/edit`)
   await page.click('text=Configure...')
   await page.click('input[placeholder="Select a typebot"]')
-  await page.click('text=Another typebot')
-  await expect(page.locator('input[value="Another typebot"]')).toBeVisible()
+  await page.click('text=My link typebot 2')
+  await expect(page.locator('input[value="My link typebot 2"]')).toBeVisible()
   await page.click('[aria-label="Navigate to typebot"]')
   await expect(page).toHaveURL(
     `/typebots/${linkedTypebotId}/edit?parentId=${typebotId}`
   )
   await page.click('[aria-label="Navigate back"]')
   await expect(page).toHaveURL(`/typebots/${typebotId}/edit`)
-  await page.click('text=Jump in Another typebot')
-  await expect(page.locator('input[value="Another typebot"]')).toBeVisible()
+  await page.click('text=Jump in My link typebot 2')
+  await expect(page.locator('input[value="My link typebot 2"]')).toBeVisible()
   await page.click('input[placeholder="Select a block"]')
   await page.click('text=Block #2')
 
@@ -36,7 +36,7 @@ test('should be configurable', async ({ page }) => {
   await expect(typebotViewer(page).locator('text=Second block')).toBeVisible()
 
   await page.click('[aria-label="Close"]')
-  await page.click('text=Jump to Block #2 in Another typebot')
+  await page.click('text=Jump to Block #2 in My link typebot 2')
   await page.click('input[value="Block #2"]', { clickCount: 3 })
   await page.press('input[value="Block #2"]', 'Backspace')
   await page.click('button >> text=Start')
@@ -47,10 +47,10 @@ test('should be configurable', async ({ page }) => {
   await expect(typebotViewer(page).locator('text=Hello there!')).toBeVisible()
 
   await page.click('[aria-label="Close"]')
-  await page.click('text=Jump to Start in Another typebot')
-  await page.click('input[value="Another typebot"]', { clickCount: 3 })
-  await page.press('input[value="Another typebot"]', 'Backspace')
-  await page.click('button >> text=My typebot')
+  await page.click('text=Jump to Start in My link typebot 2')
+  await page.click('input[value="My link typebot 2"]', { clickCount: 3 })
+  await page.press('input[value="My link typebot 2"]', 'Backspace')
+  await page.click('button >> text=My link typebot 1')
   await page.click('input[placeholder="Select a block"]', {
     clickCount: 3,
   })
