f9c16ce4c4382a24af04f4f6517c8c11a6af551f
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-08-17 07:17:09 +0200

    fix(bot): pay button not submitting

--- packages/bot-engine/src/components/ChatGroup/ChatBlock/inputs/PaymentForm/StripePaymentForm.tsx
+++ packages/bot-engine/src/components/ChatGroup/ChatBlock/inputs/PaymentForm/StripePaymentForm.tsx
@@ -175,6 +175,7 @@ const CheckoutForm = ({
       />
       {isPayButtonVisible && (
         <SendButton
+          type="submit"
           label={`${options.labels.button} ${amountLabel}`}
           isDisabled={isLoading || !stripe || !elements}
           isLoading={isLoading}
