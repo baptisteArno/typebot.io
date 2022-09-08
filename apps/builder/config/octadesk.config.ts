577f01570bbe39bb1cf1902ea6e943883b0f0b7c
Marcos Garcia <markonesgava@gmail.com>
2022-09-08 16:18:35 -0300

    fix: include new env var is local

--- apps/builder/config/octadesk.config.ts
+++ apps/builder/config/octadesk.config.ts
@@ -1,9 +1,11 @@
 type OctadeskConfig = {
     basePath: String,
     environment: String
+    local: Boolean
 }
 
 const config: OctadeskConfig = {
+    local: process.env.IS_LOCAL === 'true',
     basePath: process.env.IS_LOCAL === 'true' ? '' : '/embed/builder',
     environment: process.env.NODE_ENV_OCTADESK === undefined ? 'production' : process.env.NODE_ENV_OCTADESK,
 }
