52387d4b580f7802fcbc1930b8d85b323376d3de
Marcos Garcia <markonesgava@gmail.com>
2022-09-21 17:51:11 -0300

    fix: get config from server side

--- apps/builder/pages/api/config.ts
+++ apps/builder/pages/api/config.ts
@@ -5,13 +5,12 @@ import { methodNotAllowed } from 'utils'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     return res.send({
-      local: process.env.IS_LOCAL === 'true',
-      basePath: process.env.IS_LOCAL === 'true' ? '' : '/embed/builder',
-      environment: process.env.NODE_ENV_OCTADESK === undefined ? 'production' : process.env.NODE_ENV_OCTADESK,
-      viewerURL: process.env.IS_LOCAL === 'true' ? process.env.NEXT_PUBLIC_VIEWER_URL ? process.env.NEXT_PUBLIC_VIEWER_URL : '' : '/embed/viewer',
+      basePath: process.env.BASE_PATH,
+      environment: process.env.NODE_ENV_OCTADESK,
+      viewerURL: process.env.NEXT_PUBLIC_VIEWER_URL
     })
   }
-  
+
   return methodNotAllowed(res)
 }
 
