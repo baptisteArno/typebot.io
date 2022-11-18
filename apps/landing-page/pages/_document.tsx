d2f617ff1e831fa4b03c7a15ef45a9b7a44134fc
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2022-11-18 21:26:13 +0100

    :ambulance: (lp) Temporarily disable plausible

--- apps/landing-page/pages/_document.tsx
+++ apps/landing-page/pages/_document.tsx
@@ -20,11 +20,11 @@ class MyDocument extends Document {
             {/* eslint-disable-next-line @next/next/no-css-tags */}
             <link href="./styles/aos-noscript.css" rel="stylesheet" />
           </noscript>
-          <script
+          {/* <script
             defer
             data-domain="typebot.io"
             src="https://plausible.server.baptistearno.com/js/plausible.js"
-          ></script>
+          ></script> */}
           {/* eslint-disable-next-line @next/next/no-sync-scripts */}
           <script src="/__env.js" />
         </Head>
