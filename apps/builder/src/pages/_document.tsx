435edd03c09073442fdd19fb32157bd21e2c16ff
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-02-15 16:39:53 +0100

    :lipstick: Add proper italic fonts in bot

--- apps/builder/src/pages/_document.tsx
+++ apps/builder/src/pages/_document.tsx
@@ -7,7 +7,7 @@ const Document = () => (
     <Head>
       <link rel="icon" type="image/png" href="/favicon.png" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,600&display=swap"
         rel="stylesheet"
       />
       <meta name="google" content="notranslate" />
--- packages/bot-engine/src/components/TypebotViewer.tsx
+++ packages/bot-engine/src/components/TypebotViewer.tsx
@@ -91,7 +91,7 @@ export const TypebotViewer = ({
           dangerouslySetInnerHTML={{
             __html: `@import url('https://fonts.googleapis.com/css2?family=${
               typebot.theme.general.font ?? 'Open Sans'
-            }:wght@300;400;600&display=swap');`,
+            }:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap');`,
           }}
         />
       )}
