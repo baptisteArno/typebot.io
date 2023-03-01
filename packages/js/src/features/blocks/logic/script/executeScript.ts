cc07389c37e1008dc5041bb22162bfc69fd5eac4
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-03-01 14:33:59 +0100

    :children_crossing: (js) Parse script to content to remove useless script tags if any

--- packages/js/src/features/blocks/logic/script/executeScript.ts
+++ packages/js/src/features/blocks/logic/script/executeScript.ts
@@ -1,10 +1,17 @@
 import type { ScriptToExecute } from 'models'
 
 export const executeScript = async ({ content, args }: ScriptToExecute) => {
-  const func = Function(...args.map((arg) => arg.id), content)
+  const func = Function(...args.map((arg) => arg.id), parseContent(content))
   try {
     await func(...args.map((arg) => arg.value))
   } catch (err) {
     console.error(err)
   }
+}
+
+const parseContent = (content: string) => {
+  const contentWithoutScriptTags = content
+    .replace(/<script>/g, '')
+    .replace(/<\/script>/g, '')
+  return contentWithoutScriptTags
 }
