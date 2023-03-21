90cb0750b848937d2d591c19ebf0f3e32f3c1085
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-03-21 13:58:05 +0100

    :green_heart: Fix Sentry sourcemap upload
    Closes #405

--- apps/viewer/sentry.server.config.js
+++ apps/viewer/sentry.server.config.js
@@ -1,7 +1,3 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
