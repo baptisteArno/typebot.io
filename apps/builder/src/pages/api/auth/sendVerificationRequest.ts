4986ec79f5f9714fc89f753779f17396a719334a
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-03-29 08:14:35 +0200

    :bug: (auth) Fix invalid redirects to internal url
    Closes #403

--- apps/builder/src/pages/api/auth/sendVerificationRequest.ts
+++ apps/builder/src/pages/api/auth/sendVerificationRequest.ts
@@ -2,18 +2,11 @@ import { sendMagicLinkEmail } from '@typebot.io/emails'
 
 type Props = {
   identifier: string
-  token: string
+  url: string
 }
 
-export const sendVerificationRequest = async ({ identifier, token }: Props) => {
+export const sendVerificationRequest = async ({ identifier, url }: Props) => {
   try {
-    const url = `${
-      process.env.NEXTAUTH_URL
-    }/api/auth/callback/email?${new URLSearchParams({
-      email: identifier,
-      token,
-      callbackUrl: `${process.env.NEXTAUTH_URL}/signin`,
-    }).toString()}`
     await sendMagicLinkEmail({ url, to: identifier })
   } catch (err) {
     throw new Error(`Email(s) could not be sent`)
