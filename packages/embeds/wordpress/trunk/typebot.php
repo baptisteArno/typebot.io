c1f3534374d5f5ad8d492763b538179faec27ac9
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-04-28 16:53:28 +0200

    :adhesive_bandage: (wp) Fix typebotWpUser not defined issue

--- packages/embeds/wordpress/trunk/typebot.php
+++ packages/embeds/wordpress/trunk/typebot.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Typebot
  * Description:       Convert more with conversational forms
- * Version:           3.1.7
+ * Version:           3.1.9
  * Author:            Typebot
  * Author URI:        http://typebot.io/
  * License:           GPL-2.0+
@@ -16,7 +16,7 @@ if (!defined('WPINC')) {
   die();
 }
 
-define('TYPEBOT_VERSION', '3.1.7');
+define('TYPEBOT_VERSION', '3.1.9');
 
 function activate_typebot()
 {
