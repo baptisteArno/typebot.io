2cb30bfcc0dd1ea5d3d3023fc6704358a4424555
makemouse <132714533+makemouse@users.noreply.github.com>
2023-05-06 19:30:23 -0400

    .

--- apps/builder/src/features/workspace/helpers/parseWorkspaceDefaultPlan.ts
+++ apps/builder/src/features/workspace/helpers/parseWorkspaceDefaultPlan.ts
@@ -1,9 +1,9 @@
 import { Plan } from '@typebot.io/prisma'
 
 export const parseWorkspaceDefaultPlan = (userEmail: string) => {
-  if (process.env.ADMIN_EMAIL === userEmail) return Plan.UNLIMITED
-  const defaultPlan = process.env.DEFAULT_WORKSPACE_PLAN as Plan | undefined
-  if (defaultPlan && Object.values(Plan).includes(defaultPlan))
-    return defaultPlan
-  return Plan.FREE
+  // if (process.env.ADMIN_EMAIL === userEmail) return Plan.UNLIMITED
+  // const defaultPlan = process.env.DEFAULT_WORKSPACE_PLAN as Plan | undefined
+  // if (defaultPlan && Object.values(Plan).includes(defaultPlan))
+  //   return defaultPlan
+  return Plan.PRO
 }
