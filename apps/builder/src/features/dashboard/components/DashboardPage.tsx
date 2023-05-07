2b3740ed0a501a86bec89b35e9427473ac06705d
makemouse <132714533+makemouse@users.noreply.github.com>
2023-05-06 20:24:33 -0400

    Add files via upload

--- apps/builder/src/features/dashboard/components/DashboardPage.tsx
+++ apps/builder/src/features/dashboard/components/DashboardPage.tsx
@@ -15,35 +15,52 @@ import { DashboardHeader } from './DashboardHeader'
 import { FolderContent } from '@/features/folders/components/FolderContent'
 import { TypebotDndProvider } from '@/features/folders/TypebotDndProvider'
 import { ParentModalProvider } from '@/features/graph/providers/ParentModalProvider'
+import { trpc } from '@/lib/trpc'
 
 export const DashboardPage = () => {
   const scopedT = useScopedI18n('dashboard')
   const [isLoading, setIsLoading] = useState(false)
-  const { query } = useRouter()
+  const router = useRouter()
   const { user } = useUser()
   const { workspace } = useWorkspace()
   const [preCheckoutPlan, setPreCheckoutPlan] =
     useState<PreCheckoutModalProps['selectedSubscription']>()
+  const { mutate: createCustomCheckoutSession } =
+    trpc.billing.createCustomCheckoutSession.useMutation({
+      onSuccess: (data) => {
+        router.push(data.checkoutUrl)
+      },
+    })
 
-  // useEffect(() => {
-  //   const { subscribePlan, chats, storage, isYearly } = query as {
-  //     subscribePlan: Plan | undefined
-  //     chats: string | undefined
-  //     storage: string | undefined
-  //     isYearly: string | undefined
-  //   }
-  //   if (workspace && subscribePlan && user && workspace.plan === 'FREE') {
-  //     setIsLoading(true)
-  //     setPreCheckoutPlan({
-  //       plan: subscribePlan as 'PRO' | 'STARTER',
-  //       workspaceId: workspace.id,
-  //       additionalChats: chats ? parseInt(chats) : 0,
-  //       additionalStorage: storage ? parseInt(storage) : 0,
-  //       currency: guessIfUserIsEuropean() ? 'eur' : 'usd',
-  //       isYearly: isYearly === 'false' ? false : true,
-  //     })
-  //   }
-  // }, [query, user, workspace])
+  useEffect(() => {
+    const { subscribePlan, chats, storage, isYearly, claimCustomPlan } =
+      router.query as {
+        subscribePlan: Plan | undefined
+        chats: string | undefined
+        storage: string | undefined
+        isYearly: string | undefined
+        claimCustomPlan: string | undefined
+      }
+    if (claimCustomPlan && user?.email && workspace) {
+      setIsLoading(true)
+      createCustomCheckoutSession({
+        email: user.email,
+        workspaceId: workspace.id,
+        returnUrl: `${window.location.origin}/typebots`,
+      })
+    }
+    if (workspace && subscribePlan && user && workspace.plan === 'FREE') {
+      setIsLoading(true)
+      setPreCheckoutPlan({
+        plan: subscribePlan as 'PRO' | 'STARTER',
+        workspaceId: workspace.id,
+        additionalChats: chats ? parseInt(chats) : 0,
+        additionalStorage: storage ? parseInt(storage) : 0,
+        currency: guessIfUserIsEuropean() ? 'eur' : 'usd',
+        isYearly: isYearly === 'false' ? false : true,
+      })
+    }
+  }, [createCustomCheckoutSession, router.query, user, workspace])
 
   return (
     <Stack minH="100vh">
