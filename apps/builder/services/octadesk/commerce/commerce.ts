5218d392f8c1bcd69e55c8fed196169d715e24e7
Marcos Garcia <markonesgava@gmail.com>
2023-05-11 18:50:10 -0300

    fix: using storage headers

--- apps/builder/services/octadesk/commerce/commerce.ts
+++ apps/builder/services/octadesk/commerce/commerce.ts
@@ -10,11 +10,7 @@ export const CommerceService: CommerceServicesType = {
   getList: async (): Promise<Array<ListCatalogType>> => {
     try {
       const { data }: any = await getCommerceClient().then(client => (
-        client.get('/catalogs', { headers: {
-          'Content-type': "",
-          appsubdomain: Storage.getItem('company'),
-          authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJkb21haW4iOiJxYXMzMTkxMzYtM2E5Iiwicm9sZSI6IjEiLCJyb2xlVHlwZSI6IjEiLCJlbWFpbCI6Imx1Y2FzY29yZGVpcm9hcmF1am9AZ21haWwuY29tIiwibmFtZSI6IjE5MTIyMjE1MDkiLCJ0eXBlIjoiMSIsImlkIjoiZGJhZWRhODYtYzg5NC00OTc2LWE0ZjktYTU4ZDM4NzU1M2VmIiwicGVybWlzc2lvblR5cGUiOiIxIiwicGVybWlzc2lvblZpZXciOiIwIiwibmJmIjoxNjcxNzUwMTgwLCJleHAiOjE3MDMyODYxODAsImlhdCI6MTY3MTc1MDE4MCwiaXNzIjoiYXBpLnFhb2N0YWRlc2suc2VydmljZXMifQ.ZZRjgezCWFZc_8PCkFH2IN1_eI5ahanPncX2YtC4dQY"
-        } })
+        client.get('/catalogs', loadParameterHeader())
       ))
       return data.results as Array<ListCatalogType>;
     } catch (ex) {
@@ -23,6 +19,9 @@ export const CommerceService: CommerceServicesType = {
   },
   getProductsInCatalog: async (id: string): Promise<Array<ProductType>> => {
     try {
+      const authStorage = Storage.getItem('auth') as any
+      const accessToken = authStorage.access_token
+
       const { data }: any = await getCommerceClient().then(client =>
         client.get(`/store/products`, {
           params: {
@@ -30,11 +29,7 @@ export const CommerceService: CommerceServicesType = {
             limit: '200',
             shouldIncludeVariants: true,
           },
-          headers: {
-            'Content-type': "",
-            appsubdomain: Storage.getItem('company'),
-            authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJkb21haW4iOiJxYXMzMTkxMzYtM2E5Iiwicm9sZSI6IjEiLCJyb2xlVHlwZSI6IjEiLCJlbWFpbCI6Imx1Y2FzY29yZGVpcm9hcmF1am9AZ21haWwuY29tIiwibmFtZSI6IjE5MTIyMjE1MDkiLCJ0eXBlIjoiMSIsImlkIjoiZGJhZWRhODYtYzg5NC00OTc2LWE0ZjktYTU4ZDM4NzU1M2VmIiwicGVybWlzc2lvblR5cGUiOiIxIiwicGVybWlzc2lvblZpZXciOiIwIiwibmJmIjoxNjcxNzUwMTgwLCJleHAiOjE3MDMyODYxODAsImlhdCI6MTY3MTc1MDE4MCwiaXNzIjoiYXBpLnFhb2N0YWRlc2suc2VydmljZXMifQ.ZZRjgezCWFZc_8PCkFH2IN1_eI5ahanPncX2YtC4dQY"
-          }
+          ...loadParameterHeader()
         })
       )
       return data.results as Array<ProductType>;
