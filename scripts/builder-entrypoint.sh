45224f9fb36f97a169ac729def4dd50c6506a2ae
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-05-12 11:21:24 -0400

    :technologist: Check required env var before starting the server
    Closes #500

--- scripts/builder-entrypoint.sh
+++ scripts/builder-entrypoint.sh
@@ -2,6 +2,28 @@
 
 ENVSH_ENV=./apps/builder/.env.production ENVSH_OUTPUT=./apps/builder/public/__env.js bash env.sh
 
+echo 'Checking if required environment variables are set and valid...'
+
+if [ -z "$DATABASE_URL" ]; then
+  echo "DATABASE_URL is not set. Exiting..."
+  exit 1
+fi
+
+if [ ${#ENCRYPTION_SECRET} -ne 32 ]; then
+  echo "ENCRYPTION_SECRET is not 32 characters long. Exiting... (To generate a valid secret: https://docs.typebot.io/self-hosting/docker#2-add-the-required-configuration)"
+  exit 1
+fi
+
+if [ -z "$NEXTAUTH_URL" ]; then
+  echo "NEXTAUTH_URL is not set. Exiting..."
+  exit 1
+fi
+
+if [ -z "$NEXT_PUBLIC_VIEWER_URL" ]; then
+  echo "NEXT_PUBLIC_VIEWER_URL is not set. Exiting..."
+  exit 1
+fi
+
 ./node_modules/.bin/prisma generate --schema=packages/prisma/postgresql/schema.prisma;
 
 echo 'Waiting 5s for db to be ready...';
