78ad79ac8ae1828a4efc368c84d415c399ebb09d
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-06-06 18:24:17 +0200

    :package: Add Cloudron package
    Closes #30

---
+++ packages/cloudron/env.default.sh
@@ -0,0 +1,4 @@
+# Make sure to change this to your own random string of 32 characters (https://docs.typebot.io/self-hosting/docker#2-add-the-required-configuration)
+export ENCRYPTION_SECRET="2GKg2i0oqWTkfc8lipjRE2weLg3R+UuI"
+export DATABASE_URL=$CLOUDRON_POSTGRESQL_URL
+export NEXTAUTH_URL=$CLOUDRON_APP_ORIGIN
---
+++ packages/cloudron/start.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -eu
+chown -R cloudron:cloudron /app/data
+
+echo "Waiting for postgres to become ready...."
+
+PG_READY="pg_isready -h $CLOUDRON_POSTGRESQL_HOST -p $CLOUDRON_POSTGRESQL_PORT"
+
+until $PG_READY
+do
+  sleep 2;
+done
+
+echo "Database ready to accept connections."
+
+if [[ ! -f "/app/data/env.sh" ]]; then
+  echo "Creating env.sh file at /app/data/env.sh"
+  cp ./env.default.sh /app/data/env.sh
+else
+  echo "Skipping env.sh file creation. /app/data/env.sh exists."
+fi
+
+echo "Sourcing env.sh file at /app/data/env.sh"
+source /app/data/env.sh
+
+echo 'Injecting environment variables into frontend...'
+ENVSH_ENV=./builder/apps/builder/.env.production ENVSH_OUTPUT=./builder/apps/builder/public/__env.js ./builder/env.sh
+ENVSH_ENV=./viewer/apps/viewer/.env.production ENVSH_OUTPUT=./viewer/apps/viewer/public/__env.js ./viewer/env.sh
+
+echo 'Checking if required environment variables are set and valid...'
+
+if [ -z "$DATABASE_URL" ]; then
+  echo "DATABASE_URL is not set. Exiting..."
+  exit 1
+fi
+
+if [ ${#ENCRYPTION_SECRET} -ne 32 ] && [ ${#ENCRYPTION_SECRET} -ne 80 ]; then
+  echo "ENCRYPTION_SECRET is not 32 characters long. Exiting... (To generate a valid secret: https://docs.typebot.io/self-hosting/docker#2-add-the-required-configuration)"
+  exit 1
+fi
+
+if [ -z "$NEXTAUTH_URL" ]; then
+  echo "NEXTAUTH_URL is not set. Exiting..."
+  exit 1
+fi
+
+./builder/node_modules/.bin/prisma generate --schema=builder/packages/prisma/postgresql/schema.prisma;
+./viewer/node_modules/.bin/prisma generate --schema=viewer/packages/prisma/postgresql/schema.prisma;
+./builder/node_modules/.bin/prisma migrate deploy --schema=builder/packages/prisma/postgresql/schema.prisma;
+
+
+cd ./builder/apps/builder
+pm2 start --name=typebot server.js
+cd /app/viewer/apps/viewer
+PORT=3001 pm2 start --name=typebot server.js
