578d0360891a7d9d8eeb9cffcd289a5a7f5cbffb
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-07-21 18:21:44 -0400

    Update Seo.tsx

--- apps/builder/src/components/Seo.tsx
+++ apps/builder/src/components/Seo.tsx
@@ -18,7 +18,7 @@ export const Seo = ({
       <meta name="title" content={title} />
       <meta property="og:title" content={title} />
       <meta property="twitter:title" content={title} />
-
+<meta name="google-site-verification" content="J3xNT6mxZKsTbgkoAGkvVk0BLrMHD5de2c_icciYj7g" />
       <meta name="description" content={description} />
       <meta property="twitter:description" content={description} />
       <meta property="og:description" content={description} />
