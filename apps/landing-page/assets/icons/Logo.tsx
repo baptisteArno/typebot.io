e7c9f045fe3d28f8051f6c755716028148b5101a
jdefranco <45438667+jdefranco@users.noreply.github.com>
2023-07-25 23:38:35 -0400

    Update Logo.tsx
    ffff

--- apps/landing-page/assets/icons/Logo.tsx
+++ apps/landing-page/assets/icons/Logo.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Image, Box } from '@chakra-ui/react'
+import { Image, Box, BoxProps } from '@chakra-ui/react'
 import newLogo from '../../img/chatworth-logo..png' // corrected path to new logo file
 
-export const Logo = (props) => (
+export const Logo = (props: BoxProps) => (
   <Box boxSize="50px" {...props}>
     <Image src={newLogo} boxSize="100%" objectFit="contain" alt="Logo" />
   </Box>
 )
+
