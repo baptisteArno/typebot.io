180dc0484349c375ba93db5cc7e967a2a8478b8f
Rodrigo Hoshino Okiyama <hoshino.rodrigo@yahoo.com.br>
2023-07-26 09:53:29 -0300

    fix: duplicate keys

--- apps/builder/components/shared/Graph/Nodes/ItemNode/ItemNodeContent/contents/OfficeHoursNodeContent/OfficeHoursNodeContent.tsx
+++ apps/builder/components/shared/Graph/Nodes/ItemNode/ItemNodeContent/contents/OfficeHoursNodeContent/OfficeHoursNodeContent.tsx
@@ -1,6 +1,11 @@
 import { Stack, Tag, Text, Flex, Wrap } from '@chakra-ui/react'
 import { useTypebot } from 'contexts/TypebotContext'
-import { Comparison, ConditionItem, ComparisonOperators, OfficeHoursItem } from 'models'
+import {
+  Comparison,
+  ConditionItem,
+  ComparisonOperators,
+  OfficeHoursItem,
+} from 'models'
 import React from 'react'
 import { Container } from './OfficeHours.style'
 
@@ -9,15 +14,15 @@ type Props = {
 }
 
 export const OfficeHoursNodeContent = ({ item }: Props) => {
-  const { typebot } = useTypebot();
+  const { typebot } = useTypebot()
 
   return (
     <Container>
-      {item.content.values.map((value) => (
-        <>
-        {value === "@OFFICE_HOURS_TRUE" && "Dentro do expediente"}
-        {value === "@OFFICE_HOURS_FALSE" && "Fora do expediente"}
-        </>
+      {item.content.values.map((value, idx) => (
+        <div key={idx}>
+          {value === '@OFFICE_HOURS_TRUE' && 'Dentro do expediente'}
+          {value === '@OFFICE_HOURS_FALSE' && 'Fora do expediente'}
+        </div>
       ))}
     </Container>
   )
