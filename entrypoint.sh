ed6dadf03fbe0d660904162d6bc99c22a598d95a
Marcos Garcia <markonesgava@gmail.com>
2023-07-27 17:36:18 -0300

    fix: entrypoint changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,10 @@ function apply_path {
     find $nextFolder \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$configValue#$envValue#g"
   done < $envFilename
 }
+
+apply_path
+
+envFilename='./.env.octadesk'
   
 apply_path
 exec "$@"
