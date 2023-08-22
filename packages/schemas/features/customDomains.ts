83352d77f5cd08c2740661dc8096d7d289f8f130
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-08-22 11:11:20 +0200

    :bug: (customDomains) Transform name to lower case before validating

--- packages/schemas/features/customDomains.ts
+++ packages/schemas/features/customDomains.ts
@@ -4,7 +4,10 @@ import { z } from 'zod'
 const domainNameRegex = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/
 
 export const customDomainSchema = z.object({
-  name: z.string().refine((name) => domainNameRegex.test(name)),
+  name: z
+    .string()
+    .transform((name) => name.toLowerCase())
+    .refine((name) => domainNameRegex.test(name)),
   workspaceId: z.string(),
   createdAt: z.date(),
 }) satisfies z.ZodType<CustomDomainInDb>
