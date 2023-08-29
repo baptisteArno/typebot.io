5b20f414c836715e3f3791607f1e93ce258994b0
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-08-29 14:33:41 +0200

    :bug: (embed) Fix env reading in embed

--- packages/env/getRuntimeVariable.ts
+++ packages/env/getRuntimeVariable.ts
@@ -6,5 +6,6 @@ const isBrowser = () => Boolean(typeof window !== 'undefined' && window.__ENV)
 
 export const getRuntimeVariable = (key: string) => {
   if (isBrowser()) return window.__ENV[key]
+  if (typeof process === 'undefined') return undefined
   return process.env[key]
 }
