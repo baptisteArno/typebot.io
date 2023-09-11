f2263db98d40dc6d5154d6ce8293e62e00d780b1
Marcos Garcia <markonesgava@gmail.com>
2023-09-11 14:54:56 -0300

    fix: remove unnused codes

--- apps/builder/components/shared/Graph/Nodes/StepNode/SettingsPopoverContent/bodies/WOZSuggestion/WOZSuggestionSettingsBody.tsx
+++ apps/builder/components/shared/Graph/Nodes/StepNode/SettingsPopoverContent/bodies/WOZSuggestion/WOZSuggestionSettingsBody.tsx
@@ -18,24 +18,6 @@ export const WOZSuggestionSettingBody = ({
   options,
   onOptionsChange,
 }: WOZSuggestionSettingBodyProps) => {
-  const handleDefaultPreferredAnswer = (e: any) => {
-    const option = e
-
-    onOptionsChange({
-      ...options,
-      preferredAnswer: option,
-    })
-  }
-
-  const items = Object.entries(WOZ_ANSWER_TYPE).map((type) => {
-    return {
-      value: type[0],
-      label: type[1],
-      key: type[0],
-      optionKey: type[0]
-    }
-  })
-
   return (
     <Stack spacing={4}>
       <iframe src="https://campanhas.octadesk.com/woz" style={{ width: '100%', height: '550px' }} />
