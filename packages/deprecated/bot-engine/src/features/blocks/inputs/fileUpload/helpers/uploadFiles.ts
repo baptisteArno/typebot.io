03129935eb8446c5abb2af1680fd0e9c4411bb69
Baptiste Arnaud <contact@baptiste-arnaud.fr>
2023-11-02 14:43:49 +0100

    :ambulance: Move cache control header into the post policy

--- packages/deprecated/bot-engine/src/features/blocks/inputs/fileUpload/helpers/uploadFiles.ts
+++ packages/deprecated/bot-engine/src/features/blocks/inputs/fileUpload/helpers/uploadFiles.ts
@@ -40,7 +40,6 @@ export const uploadFiles = async ({
       Object.entries(data.formData).forEach(([key, value]) => {
         formData.append(key, value)
       })
-      formData.append('Cache-Control', 'max-age=86400')
       formData.append('file', file)
       const upload = await fetch(data.presignedUrl, {
         method: 'POST',
