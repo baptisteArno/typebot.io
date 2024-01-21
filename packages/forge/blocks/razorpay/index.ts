fb41987d8b725e5b9e9c8bdbc74374708ecbdc60
Dinesh Kashikar <dinesh@kashikar.net>
2024-01-21 22:22:33 +0530

    updated index to include baseoptions and actions

--- packages/forge/blocks/razorpay/index.ts
+++ packages/forge/blocks/razorpay/index.ts
@@ -1,12 +1,18 @@
 import { createBlock } from '@typebot.io/forge'
 import { RazorpayLogo } from './logo'
 import { auth } from './auth'
+import { baseOptions } from './baseOptions'
+import { createQrCode } from './actions/createQrCode'
+import { createOrder } from './actions/createOrder'
+import { createPaymentButton } from './actions/createPaymentButton'
+import { verifyPayment } from './actions/verifyPayment'
 
 export const razorpay = createBlock({
   id: 'razorpay',
   name: 'Razorpay',
-  tags: [],
+  tags: ['razorpay', 'payment gateway', 'payments'],
   LightLogo: RazorpayLogo,
   auth,
-  actions: [],
+  options: baseOptions,
+  actions: [createQrCode, createOrder, createPaymentButton, verifyPayment],
 })
