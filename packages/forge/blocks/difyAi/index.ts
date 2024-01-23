392ec68e414ef596554558b9326d9f97f49ba7ca
Abdullah <abdudsoul@gmail.com>
2024-01-23 15:25:57 +0400

    test

--- packages/forge/blocks/difyAi/index.ts
+++ packages/forge/blocks/difyAi/index.ts
@@ -5,7 +5,7 @@ import { auth } from './auth'
 export const difyAi = createBlock({
   id: 'dify-ai',
   name: 'Dify.AI',
-  tags: [],
+  tags: ['dify', 'ai', 'documents', 'files', 'knowledge base'],
   LightLogo: DifyAiLogo,
   auth,
   actions: [],
