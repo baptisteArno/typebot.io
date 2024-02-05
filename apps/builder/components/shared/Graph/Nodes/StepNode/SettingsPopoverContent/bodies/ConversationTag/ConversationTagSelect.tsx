147d56506f83b57e87dca9f39b11059118237eb4
Marcos Garcia <markonesgava@gmail.com>
2024-02-05 15:05:50 -0300

    fix: show selected tags on select

--- apps/builder/components/shared/Graph/Nodes/StepNode/SettingsPopoverContent/bodies/ConversationTag/ConversationTagSelect.tsx
+++ apps/builder/components/shared/Graph/Nodes/StepNode/SettingsPopoverContent/bodies/ConversationTag/ConversationTagSelect.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import { useTypebot } from 'contexts/TypebotContext'
 import { ConversationTagOptions } from 'models'
+import { OptionType } from 'components/octaComponents/OctaSelect/OctaSelect.type'
 import { Tag } from 'services/octadesk/tags/tags.types'
 import Select from 'react-select';
 
@@ -12,6 +13,8 @@ type Props = {
 export const ConversationTagSelect = ({ onSelect, selectedTags }: Props) => {
   const { tagsList } = useTypebot();
   const tagOptions: ConversationTagOptions = { tags: new Array<Tag> };
+
+  const selectedOnTagsList = selectedTags?.map((tag: Tag) => tagsList.find(s => s._id === tag._id))
 
   const handleOnChange = (selected: any): void => {
     selected?.forEach((tg: any) => {
@@ -28,7 +31,7 @@ export const ConversationTagSelect = ({ onSelect, selectedTags }: Props) => {
     <Select
       isMulti
       placeholder="Selecione uma tag"
-      defaultValue={selectedTags}
+      defaultValue={selectedOnTagsList}
       onChange={handleOnChange}
       options={tagsList}
       closeMenuOnSelect={false}
