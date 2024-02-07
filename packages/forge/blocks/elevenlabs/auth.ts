d1ff87e4a6da47f3135ad5a02ee6f50d5fed9832
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-02-07 17:30:49 +0100

    :sparkles: Add ElevenLabs block
    Closes #853

---
+++ packages/forge/blocks/elevenlabs/auth.ts
@@ -0,0 +1,15 @@
+import { option, AuthDefinition } from '@typebot.io/forge'
+
+export const auth = {
+  type: 'encryptedCredentials',
+  name: 'ElevenLabs account',
+  schema: option.object({
+    apiKey: option.string.layout({
+      label: 'API key',
+      isRequired: true,
+      inputType: 'password',
+      helperText:
+        'You can generate an API key in your ElevenLabs dashboard in the Profile menu.',
+    }),
+  }),
+} satisfies AuthDefinition
