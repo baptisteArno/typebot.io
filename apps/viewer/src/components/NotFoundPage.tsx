9b3343085191a9be896df92c5e300daf27570cef
Princewill Ochui <ochuiprincewill411@gmail.com>
2024-02-12 15:25:16 +0100

    custom 404

--- apps/viewer/src/components/NotFoundPage.tsx
+++ apps/viewer/src/components/NotFoundPage.tsx
@@ -11,8 +11,10 @@ export const NotFoundPage = () => (
       // backgroundColor: '#add8e6',
     }}
   >
-    <h1 style={{ fontWeight: 'bold', fontSize: '30px' }}>404</h1>
-    <h2>The bot you&apos;re looking for doesn&apos;t exist</h2>
+    <h1 style={{ fontWeight: 'bold', fontSize: '30px' }}>
+      {env.NEXT_PUBLIC_VIEWER_404_TITLE}
+    </h1>
+    <h2>{env.NEXT_PUBLIC_VIEWER_404_SUBTITLE}</h2>{' '}
     <a
       href="https://app.flowdacity.com"
       target="_blank"
@@ -27,9 +29,5 @@ export const NotFoundPage = () => (
     >
       Create your chatbot
     </a>
-    <h1 style={{ fontWeight: 'bold', fontSize: '30px' }}>
-      {env.NEXT_PUBLIC_VIEWER_404_TITLE}
-    </h1>
-    <h2>{env.NEXT_PUBLIC_VIEWER_404_SUBTITLE}</h2>
   </div>
 )
