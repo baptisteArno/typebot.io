87111d2cb221ea326d62642cde27e58990ab1ebe
Ochui, Princewill Patrick <ochuiprincewill411@gmail.com>
2024-02-14 15:11:07 +0100

    Added docs

--- packages/forge/blocks/razorpay/actions/createQrCode.ts
+++ packages/forge/blocks/razorpay/actions/createQrCode.ts
@@ -17,29 +17,29 @@ export const createQrCode = createAction({
     amount: option.string.layout({
       label: 'Amount',
       moreInfoTooltip: 'Amount to be paid',
-      isRequired: true
+      isRequired: true,
     }),
     uid: option.string.layout({
       label: 'Transaction ID',
-      moreInfoTooltip: 'Any unique id for the transaction'
+      moreInfoTooltip: 'Any unique id for the transaction',
     }),
     usage: option.enum(['single_use', 'multiple_use']).layout({
       accordion: 'Advanced Configuration',
       label: 'Usage',
       moreInfoTooltip: 'Will the qr code be used once or multiple times',
-      defaultValue: 'single_use'
+      defaultValue: 'single_use',
     }),
     fixed_amount: option.boolean.layout({
       accordion: 'Advanced Configuration',
       label: 'Fixed Amount',
       moreInfoTooltip: 'Is the amount fixed',
-      defaultValue: true
+      defaultValue: true,
     }),
     valid_till: option.string.layout({
       accordion: 'Advanced Configuration',
       label: 'Valid Till',
       moreInfoTooltip: 'How long is the QR code valid till (in seconds)',
-      defaultValue: '900'
+      defaultValue: '900',
     }),
     responseMapping: option.saveResponseArray(['QR Code', 'UPI Link']).layout({
       accordion: 'Save response',
@@ -56,7 +56,6 @@ export const createQrCode = createAction({
       if (!options.valid_till) options.valid_till = '900'
 
       const setMapping = async (upi: string) => {
-
         const url = await uploadFileToBucket({
           file: await generateQrCodeBuffer(upi),
           key: `tmp/qrcodes/${createId() + createId()}.png`,
@@ -69,35 +68,39 @@ export const createQrCode = createAction({
           const item = mapping.item ?? 'QR Code'
           if (item === 'QR Code') variables.set(mapping.variableId, url)
 
-          if (item === 'UPI Link')
-            variables.set(mapping.variableId, upi)
+          if (item === 'UPI Link') variables.set(mapping.variableId, upi)
         })
       }
 
       const delta = parseInt(options.valid_till)
-      const close_by = delta > 900 ? Math.floor(Date.now() / 1000) + delta : Math.floor(Date.now() / 1000) + 900
+      const close_by =
+        delta > 900
+          ? Math.floor(Date.now() / 1000) + delta
+          : Math.floor(Date.now() / 1000) + 900
       const notes: Record<string, string> = {}
       notes[options.uidLabel ?? 'vdsid'] = options.uid ?? `${close_by}`
       try {
         const qrcode = {
-          type: "upi_qr",
+          type: 'upi_qr',
           name: options.companyName ?? 'QR Code',
           usage: options.usage ?? 'single_use',
           fixed_amount: options.fixed_amount ?? true,
           payment_amount: convertToPaise(options.amount),
           description: `For ${options.companyName ?? 'Payment'}`,
           close_by: close_by,
-          notes: notes
+          notes: notes,
         }
 
-        const response: Record<string, string> = await got.post(`${apiBaseUrl}/payments/qr_codes`, {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          username: options.keyId,
-          password: credentials.keySecret,
-          json: qrcode,
-        }).json()
+        const response: Record<string, string> = await got
+          .post(`${apiBaseUrl}/payments/qr_codes`, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            username: options.keyId,
+            password: credentials.keySecret,
+            json: qrcode,
+          })
+          .json()
 
         const upi = response.image_content ?? 'Invalid Data'
 
@@ -105,9 +108,10 @@ export const createQrCode = createAction({
       } catch (error) {
         console.log('error', error)
         await setMapping('Invalid Data')
-        return logs.add('An unknown error occurred. Please check your server logs')
+        return logs.add(
+          'An unknown error occurred. Please check your server logs'
+        )
       }
-
     },
   },
 })
