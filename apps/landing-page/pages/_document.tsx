59a10f6157e50174445a5b5eda65965d9987de4b
Princewill Ochui <ochuiprincewill411@gmail.com>
2024-02-21 22:11:44 +0100

    fix favicon

--- apps/landing-page/pages/_document.tsx
+++ apps/landing-page/pages/_document.tsx
@@ -9,6 +9,27 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" type="image/png" href="/favicon.ico" />
           <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#275af2" />
+          <meta name="msapplication-TileColor" content="#ffffff"></meta>
+          <meta name="theme-color" content="#ffffff"></meta>
+          <link
             href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700&family=Indie+Flower:wght@400&display=swap"
             rel="stylesheet"
           />
