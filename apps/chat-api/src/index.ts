e458c8930eb2340d0a3a9149219299c46ac03bab
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-02-26 11:01:22 +0100

    :loud_sound: Debug env not propagating

--- apps/chat-api/src/index.ts
+++ apps/chat-api/src/index.ts
@@ -8,6 +8,8 @@ app.get('/ping', (c) => c.json({ status: 'ok' }, 200))
 app.route('/', webRuntime)
 app.route('/', whatsAppRuntime)
 
+console.log(process.env.DATABASE_URL, process.env.SMTP_USERNAME)
+
 export default {
   port: process.env.PORT ?? 3002,
   fetch: app.fetch,
