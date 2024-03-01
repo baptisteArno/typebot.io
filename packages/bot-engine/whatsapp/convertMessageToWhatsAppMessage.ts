3971e4a888a97c2017ae243c903c981e9e592900
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-03-01 16:24:36 +0100

    :children_crossing: (whatsapp) Remove .mp4 regex restriction for video URLs
    Closes #1282

--- packages/bot-engine/whatsapp/convertMessageToWhatsAppMessage.ts
+++ packages/bot-engine/whatsapp/convertMessageToWhatsAppMessage.ts
@@ -5,8 +5,6 @@ import { BubbleBlockType } from '@typebot.io/schemas/features/blocks/bubbles/con
 import { VideoBubbleContentType } from '@typebot.io/schemas/features/blocks/bubbles/video/constants'
 import { convertRichTextToMarkdown } from '@typebot.io/lib/markdown/convertRichTextToMarkdown'
 
-const mp4HttpsUrlRegex = /^https:\/\/.*\.mp4$/
-
 export const convertMessageToWhatsAppMessage = (
   message: ContinueChatResponse['messages'][number]
 ): WhatsAppSendingMessage | undefined => {
@@ -45,8 +43,7 @@ export const convertMessageToWhatsAppMessage = (
     case BubbleBlockType.VIDEO: {
       if (
         !message.content.url ||
-        (message.content.type !== VideoBubbleContentType.URL &&
-          isVideoUrlNotCompatible(message.content.url))
+        message.content.type !== VideoBubbleContentType.URL
       )
         return
       return {
