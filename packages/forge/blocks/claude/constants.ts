5b6b5be033d75078d169900d88358368004aae50
Retr0-01 <contact@retr0.dev>
2024-03-06 14:35:33 +0200

    update anthropic sdk to support claude 3

--- packages/forge/blocks/claude/constants.ts
+++ packages/forge/blocks/claude/constants.ts
@@ -1,11 +1,11 @@
 export const claudeModels = [
+  'claude-3-opus-20240229',
   'claude-2.1',
   'claude-2.0',
   'claude-instant-1.2',
 ] as const
 
 export const defaultClaudeOptions = {
-  baseUrl: 'https://api.openai.com/v1',
   model: claudeModels[0],
   temperature: 1,
   maxTokens: 1024,
