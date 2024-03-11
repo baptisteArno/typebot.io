4dd1e28fe991d63ce893351c48e26c06b3f82ee5
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-03-11 18:25:38 +0100

    :chart_with_upwards_trend: Add prometheus metrics

--- apps/chat-api/src/index.ts
+++ apps/chat-api/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono'
 import { webRuntime } from './runtimes/web'
 import { whatsAppRuntime } from './runtimes/whatsapp'
+import { prometheus } from '@hono/prometheus'
 
 const app = new Hono()
+
+const { printMetrics, registerMetrics } = prometheus()
+app.use('*', registerMetrics)
+app.get('/metrics', printMetrics)
 
 app.get('/ping', (c) => c.json({ status: 'ok' }, 200))
 app.route('/', webRuntime)
