9839f3cffe4a1c91c79a121dc25ac6cdfe31a1dd
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-04-05 14:38:45 +0200

    :loud_sound: Add duration for debugging purpose

--- packages/lib/prisma.ts
+++ packages/lib/prisma.ts
@@ -6,15 +6,59 @@ let prisma: PrismaClient
 
 if (env.NODE_ENV === 'production' && !process.versions.bun) {
   prisma = new PrismaClient({
-    log: ['info', 'warn', 'error'],
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'info',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  })
+  //@ts-ignore
+  prisma.$on('query', (e: { query: string; duration: string }) => {
+    console.log('Query: ' + e.query)
+    console.log('Duration: ' + e.duration + 'ms')
   })
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient({
-      log: ['info', 'warn', 'error'],
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'stdout',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
     })
   }
   prisma = global.prisma
+  //@ts-ignore
+  prisma.$on('query', (e: { query: string; duration: string }) => {
+    console.log('Query: ' + e.query)
+    console.log('Duration: ' + e.duration + 'ms')
+  })
 }
 
 export default prisma
