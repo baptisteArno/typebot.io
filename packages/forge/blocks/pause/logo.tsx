d2f79b28ad89fe87b43464bc3dd0af41a77198c1
Héron Silva <heronsilva@instant.com.br>
2024-04-18 13:01:13 -0300

    Fix pause logo

--- packages/forge/blocks/pause/logo.tsx
+++ packages/forge/blocks/pause/logo.tsx
@@ -1,7 +1,12 @@
 /** @jsxImportSource react */
 
 export const PauseLogo = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 256 233" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    {...props}
+  >
     <path
       fill-rule="evenodd"
       d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
