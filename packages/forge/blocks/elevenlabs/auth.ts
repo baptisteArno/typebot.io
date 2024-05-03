8a27cea36b5a32cf2e550e3e3fd48b12e41cb96d
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-05-03 08:34:48 +0200

    :children_crossing: (elevenlabs) Remove variable button in cred…

--- packages/forge/blocks/elevenlabs/auth.ts
+++ packages/forge/blocks/elevenlabs/auth.ts
@@ -11,6 +11,7 @@ export const auth = {
       helperText:
         'You can generate an API key in your ElevenLabs dashboard in the Profile menu.',
       isDebounceDisabled: true,
+      withVariableButton: false,
     }),
   }),
 } satisfies AuthDefinition
