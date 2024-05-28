d51c20cfb0c2eac617604c3ebd3890d77fd005b4
Paulo Wender <opaulowender@gmail.com>
2024-05-28 15:14:00 -0400

    default lang pt-BR

--- apps/builder/src/lib/tolgee.tsx
+++ apps/builder/src/lib/tolgee.tsx
@@ -15,9 +15,9 @@ export const tolgee = Tolgee()
   .init({
     apiKey: env.NEXT_PUBLIC_TOLGEE_API_KEY,
     apiUrl: env.NEXT_PUBLIC_TOLGEE_API_URL,
-    defaultLanguage: 'en',
+    defaultLanguage: 'pt-BR',
     availableLanguages: ['en', 'fr', 'de', 'pt', 'pt-BR', 'es', 'ro', 'it'],
-    fallbackLanguage: 'en',
+    fallbackLanguage: 'pt-BR',
     staticData: {
       en,
       fr,
