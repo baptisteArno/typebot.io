95ff53156b1a75d8d26270a60c3d730e64b82bd2
Paulo Wender <opaulowender@gmail.com>
2024-05-28 15:13:05 -0400

    default lang pt-BR

--- apps/builder/next.config.mjs
+++ apps/builder/next.config.mjs
@@ -44,7 +44,7 @@ const nextConfig = {
     '@typebot.io/env',
   ],
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'pt-BR',
     locales: ['en', 'fr', 'pt', 'pt-BR', 'de', 'ro', 'es', 'it'],
   },
   experimental: {
