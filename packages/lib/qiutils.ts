fb3c2c41f7bab640d988ae61e6d9789a6e25f084
Paulo Wender <opaulowender@gmail.com>
2024-05-31 17:43:50 -0400

    disable integrations

---
+++ packages/lib/qiutils.ts
@@ -0,0 +1,59 @@
+// Bubbles to be disabled
+const disabledBubbles = [
+  // 'text',
+  'image',
+  'video',
+  'embed',
+  'audio',
+] as string[]
+
+// Inputs to be disabled
+const disabledInputs = [
+  // 'text input',
+  // 'number input',
+  // 'email input',
+  // 'url input',
+  'date input',
+  // 'phone number input',
+  'choice input',
+  'picture choice input',
+  'payment input',
+  // 'rating input',
+  'file input',
+] as string[]
+
+// Logic to be disabled
+const disabledLogic = [
+  // 'Set variable',
+  // 'Condition',
+  // 'Redirect',
+  // 'Code',
+  // 'Typebot link',
+  // 'Wait',
+  // 'Jump',
+  // 'AB test',
+] as string[]
+
+// Integrations to be disabled
+const disabledIntegrations = [
+  'Google Sheets',
+  // 'OpenAI',
+  'Google Analytics',
+  // 'Webhook',
+  'Email',
+  'Zapier',
+  // 'Make.com',
+  'Pabbly',
+  'Chatwoot',
+  'Pixel',
+  'Zemantic AI',
+]
+// TODO: remove from here to enable blocks
+export const disabledBlocks = [
+  ...disabledBubbles,
+  ...disabledInputs,
+  ...disabledLogic,
+  ...disabledIntegrations,
+]
+
+export const isEnabledBlock = (type: any) => !disabledBlocks.includes(type)
