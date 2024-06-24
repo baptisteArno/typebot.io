2a8dc3a239ffb9a00135daa7f8331f73a9aa367d
k1dav <code@k1dav.fun>
2024-06-24 17:24:16 +0000

    fix(const): litellm

--- packages/forge/blocks/litellm/constants.ts
+++ packages/forge/blocks/litellm/constants.ts
@@ -8,9 +8,9 @@ export const openAIVoices = [
 ] as const
 
 export const modelsWithImageUrlSupport = [
-  'gpt-4-turbo*',
-  'gpt-4o*',
-  'gpt-4*vision',
+  '*gpt-4-turbo*',
+  '*gpt-4o*',
+  '*gpt-4*vision',
 ]
 
 export const excludedModelsFromImageUrlSupport = ['gpt-4-turbo-preview']
