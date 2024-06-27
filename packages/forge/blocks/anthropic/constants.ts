99eae33187c8a143e232d5c188769db0d7b31c9b
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-06-27 11:43:21 +0200

    :zap: (anthropic) Add Claude 3.5 model

--- packages/forge/blocks/anthropic/constants.ts
+++ packages/forge/blocks/anthropic/constants.ts
@@ -1,4 +1,5 @@
 export const anthropicModels = [
+  'claude-3-5-sonnet-20240620	',
   'claude-3-opus-20240229',
   'claude-3-sonnet-20240229',
   'claude-3-haiku-20240307',
@@ -8,7 +9,7 @@ export const anthropicModels = [
 ] as const
 
 export const defaultAnthropicOptions = {
-  model: anthropicModels[0],
+  model: 'claude-3-opus-20240229',
   temperature: 1,
   maxTokens: 1024,
 } as const
