a66182b950e4e3eacc2fe9bf6429912df2737129
Gabriel Bolner <97565067+gabrielbolner@users.noreply.github.com>
2024-09-10 11:12:27 -0300

    :sparkles: Add topic send on n2 handover

--- packages/forge/blocks/claudia/actions/forward-to-human.ts
+++ packages/forge/blocks/claudia/actions/forward-to-human.ts
@@ -1,14 +1,22 @@
-import { createAction } from '@typebot.io/forge'
+import { createAction, option } from '@typebot.io/forge'
 import { createClaudiaResponseLog } from '../helpers/createClaudiaResponseLog'
 
 export const forwardToHuman = createAction({
   name: 'Forward to Human [N2]',
   run: {
-    server: ({ logs }) => {
+    server: ({ logs, options }) => {
       const log = createClaudiaResponseLog({
         action: 'FORWARD_TO_HUMAN',
+        topic: options.topic,
       })
       logs.add(log)
     },
   },
+  options: option.object({
+    topic: option.string.layout({
+      label: 'Topic',
+      placeholder: 'e.g. PAYMENT',
+      accordion: 'Advanced settings',
+    }),
+  }),
 })
