3a82646fb9b8ba16a021e5dbd11e25e883d38d25
amrsaadabdallah <amrsaadabdallah@gmail.com>
2024-09-26 19:11:33 +0300

    fix build issue due to execute assign chat issue

--- packages/bot-engine/blocks/logic/assignChat/executeAssignChat.ts
+++ packages/bot-engine/blocks/logic/assignChat/executeAssignChat.ts
@@ -1,13 +1,18 @@
 import { SessionState, AssignChatBlock } from '@typebot.io/schemas'
 import { ExecuteLogicResponse } from '../../../types'
 import { assignChatType } from '@typebot.io/schemas/features/blocks/logic/assignChat/constants'
+import { TRPCError } from '@trpc/server'
 
 export const executeAssignChat = (
   _: SessionState,
   block: AssignChatBlock
-): ExecuteLogicResponse | void => {
+): ExecuteLogicResponse => {
   let assignType: assignChatType | undefined = block.options?.assignType
-  if (!assignType) return
+  if (!assignType)
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'You forgot to select the asssign type',
+    })
 
   const assign: AssignChatBlock['options'] = {
     assignType: assignType,
