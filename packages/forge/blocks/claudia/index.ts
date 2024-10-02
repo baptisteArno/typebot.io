468b4bcbe7c2f11e48870b6390d644f459c94815
Gabriel Bolner <gabrielbolner@hotmail.com>
2024-10-02 14:05:40 -0300

    ✨ Add option to answer ticket right after flow end n1

--- packages/forge/blocks/claudia/index.ts
+++ packages/forge/blocks/claudia/index.ts
@@ -1,13 +1,14 @@
 import { createBlock } from '@typebot.io/forge'
 import { ClaudiaLogo } from './logo'
-import { endFLow } from './actions/end-flow'
+import { endFlow } from './actions/end-flow'
 import { forwardToHuman } from './actions/forward-to-human'
 import { closeTicket } from './actions/close-ticket'
+import { answerTicket } from './actions/answer_ticket'
 
 export const claudiaBlock = createBlock({
   id: 'claudia',
   name: 'ClaudIA',
   tags: [],
   LightLogo: ClaudiaLogo,
-  actions: [endFLow, forwardToHuman, closeTicket],
+  actions: [endFlow, forwardToHuman, closeTicket, answerTicket],
 })
