86df711d6e1d783e0e1836ce38d0889e011871d2
basuabhirup <basu.abhirup27@gmail.com>
2024-11-09 01:54:22 +0530

    Fetch saved credential and prompt to execute the AI action

--- apps/builder/src/features/editor/api/generateGroupTitle.ts
+++ apps/builder/src/features/editor/api/generateGroupTitle.ts
@@ -1,5 +1,6 @@
 import { authenticatedProcedure } from "@/helpers/server/trpc";
 import { TRPCError } from "@trpc/server";
+import { decrypt } from "@typebot.io/lib/api/encryption/decrypt";
 import prisma from "@typebot.io/prisma";
 import { z } from "@typebot.io/zod";
 
@@ -9,7 +10,6 @@ export const generateGroupTitle = authenticatedProcedure
       workspaceId: z.string(),
       groupIndex: z.number(),
       groupContent: z.any(),
-      setTypebot: z.any(),
     }),
   )
   .output(z.object({ title: z.string() }))
@@ -35,10 +35,50 @@ export const generateGroupTitle = authenticatedProcedure
       });
     }
 
-    // TODO:
-    // Fetch credential using workspace.aiFeatureCredentialId
-    // Fetch prompt using workspace.aiFeaturePrompt
-    // Execute AI call to the corresponing LLM model based on the credential type
+    const credentials = await prisma.credentials.findFirst({
+      where: {
+        id: workspace.aiFeatureCredentialId,
+      },
+      select: {
+        data: true,
+        iv: true,
+        name: true,
+        type: true,
+      },
+    });
 
-    return { title: `Custom title` };
+    if (!credentials)
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Credentials not found",
+      });
+
+    const credentialsData = await decrypt(credentials.data, credentials.iv);
+    console.log({ credentials, credentialsData });
+
+    const aiProvider = credentials.type;
+    const savedPrompt = workspace.aiFeaturePrompt;
+    let generateGroupTitle = "";
+
+    switch (aiProvider) {
+      case "openai":
+        // Call OpenAI API
+        generateGroupTitle = "OpenAI title";
+        break;
+      case "open-router":
+        // Call OpenRouter API
+        generateGroupTitle = "OpenRouter title";
+        break;
+      case "anthropic":
+        // Call Anthropic API
+        generateGroupTitle = "Anthropic title";
+        break;
+      default:
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Invalid AI provider",
+        });
+    }
+
+    return { title: generateGroupTitle };
   });
