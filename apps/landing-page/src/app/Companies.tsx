ec334d41317f164054e9fa33a3f4ff064024ab3a
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-11-28 14:37:18 +0100

    fix brands gradient opacity

--- apps/landing-page/src/app/Companies.tsx
+++ apps/landing-page/src/app/Companies.tsx
@@ -28,8 +28,8 @@ export const Companies = () => (
         <Obole width="80px" height="60px" />
         <Awwwsome width="100px" height="60px" />
       </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-[50px] bg-gradient-to-r from-gray-1/20 to-gray-1 " />
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-[50px] bg-gradient-to-l from-gray-1/20 to-gray-1" />
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-[50px] bg-gradient-to-r from-gray-2/20 to-gray-2 " />
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-[50px] bg-gradient-to-l from-gray-2/20 to-gray-2" />
     </div>
   </div>
 );
