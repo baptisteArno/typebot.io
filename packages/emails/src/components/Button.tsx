1d6eca4c0c448e59e269f15a925e2b0ec7323585
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-12-01 19:31:14 +0100

    🐛 Add explicit react import in emails package to avoid not found bug in CI
    related to GH action report email not sending #1910

--- packages/emails/src/components/Button.tsx
+++ packages/emails/src/components/Button.tsx
@@ -1,10 +1,12 @@
 import type { IMjmlButtonProps } from "@faire/mjml-react";
 import { MjmlButton } from "@faire/mjml-react";
+import type { ReactNode } from "react";
+import * as React from "react";
 import { blue, borderBase, grayLight, leadingTight, textBase } from "../theme";
 
 type ButtonProps = {
   link: string;
-  children: React.ReactNode;
+  children: ReactNode;
 } & IMjmlButtonProps;
 
 export const Button = ({ link, children, ...props }: ButtonProps) => (
