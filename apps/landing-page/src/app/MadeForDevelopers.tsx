d468bb662fa52455ff974419238bd9cf59da131e
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2024-12-08 11:19:18 +0100

    responsive made for developer section

--- apps/landing-page/src/app/MadeForDevelopers.tsx
+++ apps/landing-page/src/app/MadeForDevelopers.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { CodeIcon } from "@typebot.io/ui/icons/CodeIcon";
 import { GithubIcon } from "@typebot.io/ui/icons/GithubIcon";
 import { RulerIcon } from "@typebot.io/ui/icons/RulerIcon";
@@ -28,7 +29,7 @@ const data = [
 export const MadeForDevelopers = () => {
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col md:items-center gap-4">
         <h2>Built for everyone, made for developers</h2>
         <div className="flex gap-3 items-center">
           <GithubIcon className="size-4" />
@@ -41,11 +42,18 @@ export const MadeForDevelopers = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col md:flex-row gap-6">
         {data.map(({ Icon, text }, index) => (
           <div key={index} className="flex items-start gap-3">
             <Icon className="size-4 mt-1" />
-            <p>{text}</p>
+            <p
+              className={cn(
+                "md:w-44",
+                index === data.length - 1 ? undefined : "text-balance",
+              )}
+            >
+              {text}
+            </p>
           </div>
         ))}
       </div>
