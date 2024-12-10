61e385e9a4f61f5b99a4b4062acd961b70f078f5
Vagner Silva <vagner.ess@gmail.com>
2024-12-10 14:10:21 -0300

    :loud_sound: Improve prisma logs
    Signed-off-by: Vagner Silva <vagner.ess@gmail.com>

--- packages/lib/prisma.ts
+++ packages/lib/prisma.ts
@@ -15,15 +15,16 @@ if (env.NODE_ENV === 'production' && !process.versions.bun) {
     })
   }
   prisma = global.prisma
-  prisma.$use(async (params, next) => {
-    const before = Date.now()
-    const result = await next(params)
-    const after = Date.now()
-
-    console.log(`Query took ${after - before}ms`)
-    console.log(`Params: ${JSON.stringify(params, null, 2)}`)
-    return result
-  })
 }
+
+prisma.$use(async (params, next) => {
+  const before = Date.now()
+  const result = await next(params)
+  const after = Date.now()
+
+  console.log(`Query took ${after - before}ms`)
+  console.log(`Params: ${JSON.stringify(params, null, 2)}`)
+  return result
+})
 
 export default prisma
