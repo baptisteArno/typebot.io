c6637bec97ee9411880780f1262949744bc3ab69
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2025-01-24 15:10:31 +0100

    improve auth UI/UX

--- packages/forge/blocks/posthog/src/auth.ts
+++ packages/forge/blocks/posthog/src/auth.ts
@@ -6,21 +6,17 @@ export const auth = {
   name: "Posthog account",
   schema: option.object({
     apiKey: option.string.layout({
-      label: "API key",
+      label: "Project API key",
       isRequired: true,
       inputType: "password",
-      helperText:
-        "You can find your project API key [here](https://us.posthog.com/project/YOUR_PROJECT_ID/settings/project-details).",
       withVariableButton: false,
       isDebounceDisabled: true,
     }),
     host: option.string.layout({
       label: "Host",
-      isRequired: true,
-      helperText:
-        "You can find your api host value [here](https://us.posthog.com/project/YOUR_PROJECT_ID/settings/project-details).",
       withVariableButton: false,
       isDebounceDisabled: true,
+      defaultValue: "https://us.posthog.com",
     }),
   }),
 } satisfies AuthDefinition;
