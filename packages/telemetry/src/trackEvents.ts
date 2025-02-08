9738b4d2c0a753c118fa4f1e6983033e7e5490fb
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2025-02-08 12:24:50 +0100

    📈 Re-introduce group identifies
    This is necessary for proper analytics on PostHog's side. But it is not necessary to capture the associated "create" event as well.

--- packages/telemetry/src/trackEvents.ts
+++ packages/telemetry/src/trackEvents.ts
@@ -9,6 +9,22 @@ export const trackEvents = async (events: TelemetryEvent[]) => {
   });
 
   events.forEach(async (event) => {
+    if (event.name === "Workspace created") {
+      client.groupIdentify({
+        distinctId: event.userId,
+        groupType: "workspace",
+        groupKey: event.workspaceId,
+      });
+      return;
+    }
+    if (event.name === "Typebot created") {
+      client.groupIdentify({
+        distinctId: event.userId,
+        groupType: "typebot",
+        groupKey: event.typebotId,
+      });
+      return;
+    }
     const groups: { workspace?: string; typebot?: string } = {};
     if ("workspaceId" in event) groups["workspace"] = event.workspaceId;
     if ("typebotId" in event) groups["typebot"] = event.typebotId;
