bbb90b529b2a57fe9bcbb128780fad240be4f664
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2025-02-17 15:16:46 +0100

    🐛 (whatsapp) Add "unsupported" incoming message type

--- packages/whatsapp/src/schemas.ts
+++ packages/whatsapp/src/schemas.ts
@@ -149,6 +149,9 @@ export const incomingMessageSchema = z.discriminatedUnion("type", [
       emoji: z.string().optional(),
     }),
   }),
+  sharedIncomingMessageFieldsSchema.extend({
+    type: z.literal("unsupported"),
+  }),
 ]);
 
 const incomingErrors = z.object({
