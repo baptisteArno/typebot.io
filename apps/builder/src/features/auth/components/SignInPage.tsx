62e08ded22edce52cc3fb57530d83bef893d9ccb
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2025-02-21 13:11:43 +0200

    signin

--- apps/builder/src/features/auth/components/SignInPage.tsx
+++ apps/builder/src/features/auth/components/SignInPage.tsx
@@ -1,19 +1,16 @@
 import { Seo } from "@/components/Seo";
-import { TextLink } from "@/components/TextLink";
-import {
+ import {
   Heading,
   Stack,
-  Text,
   VStack,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { T, useTranslate } from "@tolgee/react";
+import {  useTranslate } from "@tolgee/react";
 import { useRouter } from "next/router";
 import { SignInForm } from "./SignInForm";
 
 type Props = {
   type: "signin" | "signup";
-  defaultEmail?: string;
 };
 
 export const SignInPage = ({ type }: Props) => {
@@ -35,45 +32,13 @@ export const SignInPage = ({ type }: Props) => {
         rounded="lg"
         spacing={6}
       >
-        <Stack gap={4}>
           <Heading>
-            {type === "signin"
-              ? t("auth.signin.heading")
-              : t("auth.register.heading")}
+            {t("auth.signin.heading")  }
           </Heading>
-          {type === "signin" ? (
-            <Text>
-              {t("auth.signin.noAccountLabel.preLink")}{" "}
-              <TextLink href="/register">
-                {t("auth.signin.noAccountLabel.link")}
-              </TextLink>
-            </Text>
-          ) : (
-            <Text>
-              {t("auth.register.alreadyHaveAccountLabel.preLink")}{" "}
-              <TextLink href="/signin">
-                {t("auth.register.alreadyHaveAccountLabel.link")}
-              </TextLink>
-            </Text>
-          )}
-        </Stack>
+      
 
         <SignInForm defaultEmail={query.g?.toString()} />
-        {type === "signup" ? (
-          <Text fontSize="sm" maxW="330px" textAlign="center">
-            <T
-              keyName="auth.register.aggreeToTerms"
-              params={{
-                terms: (
-                  <TextLink href={"https://zazubot.com/terms-of-service"} />
-                ),
-                privacy: (
-                  <TextLink href={"https://zazubot.com/privacy-policy"} />
-                ),
-              }}
-            />
-          </Text>
-        ) : null}
+        
       </Stack>
     </VStack>
   );
--- apps/landing-page/app/constants.ts
+++ apps/landing-page/app/constants.ts
@@ -3,11 +3,11 @@ export const breakpoints = {
 };
 
 export const currentBaseUrl = "https://zazubot.com";
-export const signinUrl = "https://app.typebot.io/signin";
-export const registerUrl = "https://app.typebot.io/register";
-export const githubRepoUrl = "https://github.com/baptisteArno/typebot.io";
-export const blueskyUrl = "https://bsky.app/profile/typebot.io";
-export const linkedInUrl = "https://www.linkedin.com/company/typebot";
+export const signinUrl = "https://app.zazubot.com/signin";
+export const registerUrl = "https://app.zazubot.com/register";
+export const githubRepoUrl = "https://github.com/zazubot/typebot.io";
+export const blueskyUrl = "https://bsky.app/profile/zazubot.com";
+export const linkedInUrl = "https://www.linkedin.com/company/zazubot";
 export const discordUrl = "https://zazubot.com/discord";
 export const docsUrl = "https://docs.zazubot.com";
 export const howToGetHelpUrl = `${docsUrl}/guides/how-to-get-help`;
