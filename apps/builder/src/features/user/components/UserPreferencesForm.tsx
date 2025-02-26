713b95d0bc020f3ea5b27f641ff8cb98679f9362
Enzo Giovanni <enzingiovanni@gmail.com>
2025-02-26 19:02:08 -0300

    feat: update locale handling to set preferredLanguage in user profile

--- apps/builder/src/features/user/components/UserPreferencesForm.tsx
+++ apps/builder/src/features/user/components/UserPreferencesForm.tsx
@@ -49,7 +49,7 @@ export const UserPreferencesForm = () => {
   };
 
   const updateLocale = (locale: keyof typeof localeHumanReadable) => () => {
-    document.cookie = `NEXT_LOCALE=${locale}; path=/; max-age=31536000`;
+    updateUser({ preferredLanguage: locale });
     router.replace(
       {
         pathname: router.pathname,
