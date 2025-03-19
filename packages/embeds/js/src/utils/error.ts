f8f17a0cc497329b026941ff301d3ec6b76f0a4f
Alexis Falaise <alexis.falaise@external.engie.com>
2025-03-19 17:30:14 +0100

    enhance catchable errors list - use lowercase

--- packages/embeds/js/src/utils/error.ts
+++ packages/embeds/js/src/utils/error.ts
@@ -1,16 +1,26 @@
 export const isNetworkError = (error: Error) => {
   if (error instanceof TypeError) {
     const networkErrorMessages = [
+      "Connection refused",
       "Failed to fetch",
+      "Failed to load resource",
+      "Failed to execute 'fetch' on 'Window'",
+      "Load failed",
       "NetworkError",
       "Network request failed",
-      "Connection refused",
-      "ECONNREFUSED",
-      "timeout exceeded",
+      "Request failed",
     ];
 
-    return networkErrorMessages.some((message) =>
-      error.message.includes(message),
+    const requestErrorMessages = [
+      "ERR_INTERNET_DISCONNECTED",
+      "ERR_NAME_NOT_RESOLVED",
+      "ERR_CONNECTION_REFUSED",
+      "ERR_CONNECTION_TIMED_OUT",
+      "ERR_SSL_PROTOCOL_ERROR",
+    ];
+
+    return [...networkErrorMessages, ...requestErrorMessages].some((message) =>
+      error.message.toLowerCase().includes(message.toLowerCase()),
     );
   }
 
