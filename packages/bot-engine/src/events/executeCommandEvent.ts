39774d9e3250d164331ab1da103d704cb10e4673
Alexis Falaise <alexis.falaise@external.engie.com>
2025-04-01 16:41:52 +0200

    extract an executeEvent function

--- packages/bot-engine/src/events/executeCommandEvent.ts
+++ packages/bot-engine/src/events/executeCommandEvent.ts
@@ -2,10 +2,7 @@ import { TRPCError } from "@trpc/server";
 import type { SessionState } from "@typebot.io/chat-session/schemas";
 import { EventType } from "@typebot.io/events/constants";
 import type { CommandEvent } from "@typebot.io/events/schemas";
-import { getBlockById } from "@typebot.io/groups/helpers/getBlockById";
-import { addBlockToTypebotIfMissing } from "../addBlockToTypebotIfMissing";
-import { addPortalEdge } from "../addPortalEdge";
-import { getNextGroup } from "../getNextGroup";
+import { executeEvent } from "./executeEvent";
 
 type Props = {
   state: SessionState;
@@ -23,57 +20,9 @@ export const executeCommandEvent = async ({
       code: "BAD_REQUEST",
       message: "Command event not found",
     });
-  let newSessionState = state;
-  if (event.options?.resumeAfter && newSessionState.currentBlockId) {
-    const { block, group } = getBlockById(
-      newSessionState.currentBlockId,
-      newSessionState.typebotsQueue[0].typebot.groups,
-    );
-    if (!block)
-      throw new TRPCError({
-        code: "BAD_REQUEST",
-        message: "Block not found",
-      });
-    newSessionState = addPortalEdge(`virtual-${event.id}`, newSessionState, {
-      to: { groupId: group.id, blockId: block.id },
-    });
-    newSessionState = {
-      ...newSessionState,
-      typebotsQueue: [
-        {
-          ...newSessionState.typebotsQueue[0],
-          queuedEdgeIds: newSessionState.typebotsQueue[0].queuedEdgeIds
-            ? [
-                `virtual-${event.id}`,
-                ...newSessionState.typebotsQueue[0].queuedEdgeIds,
-              ]
-            : [`virtual-${event.id}`],
-        },
-        ...newSessionState.typebotsQueue.slice(1),
-      ],
-    };
-  }
-  const response = await getNextGroup({
-    state: newSessionState,
-    edgeId: event.outgoingEdgeId,
-    isOffDefaultPath: false,
+
+  return await executeEvent({
+    state,
+    event,
   });
-  newSessionState = response.newSessionState;
-  if (!response.group)
-    throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Command event doesn't have a connected group",
-    });
-  newSessionState = addBlockToTypebotIfMissing(
-    `virtual-${event.id}-block`,
-    newSessionState,
-    {
-      groupId: response.group.id,
-      index: 0,
-    },
-  );
-  return {
-    ...newSessionState,
-    currentBlockId: `virtual-${event.id}-block`,
-  };
 };
