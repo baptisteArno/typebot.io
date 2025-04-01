ff374968b90a3ebc6be8649aee03b5b0bf221f30
Alexis Falaise <alexis.falaise@external.engie.com>
2025-04-01 17:06:18 +0200

    update message icon

--- apps/builder/src/components/icons.tsx
+++ apps/builder/src/components/icons.tsx
@@ -736,7 +736,6 @@ export const GalleryIcon = (props: IconProps) => (
 
 export const MessageIcon = (props: IconProps) => (
   <Icon viewBox="0 0 24 24" {...featherIconsBaseProps} {...props}>
-    <line x1="22" y1="2" x2="11" y2="13"></line>
-    <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+    <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
   </Icon>
 );
