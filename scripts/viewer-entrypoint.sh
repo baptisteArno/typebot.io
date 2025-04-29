5a717126c11a56da8e09dc71dca88dab21b6cd07
silvadavitor <silvadavitor999@gmail.com>
2025-04-29 11:25:57 -0300

    fix(entrypoint): allow IPv6 binding via HOSTNAME env var (closes #2133)

--- scripts/viewer-entrypoint.sh
+++ scripts/viewer-entrypoint.sh
@@ -4,4 +4,4 @@ cd apps/viewer;
 node  -e "const { configureRuntimeEnv } = require('next-runtime-env/build/configure'); configureRuntimeEnv();"
 cd ../..;
 
-NODE_OPTIONS=--no-node-snapshot HOSTNAME=0.0.0.0 PORT=3000 node apps/viewer/server.js;
+NODE_OPTIONS=--no-node-snapshot HOSTNAME=${HOSTNAME:-0.0.0.0} PORT=${PORT:-3000} node apps/viewer/server.js;
