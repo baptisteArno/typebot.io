a6ce58198b6cf04338f8ce9daf123be3e7984fbb
Alexandre Machado <alexandre@machado.cc>
2025-06-09 18:43:23 -0300

    refactor: simplify console redirection to logger (#56)

--- apps/viewer/src/helpers/logger.ts
+++ apps/viewer/src/helpers/logger.ts
@@ -29,15 +29,12 @@ if (typeof window === 'undefined' && process.env.NODE_ENV === 'production') {
     ],
   })
 
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const util = require('util')
-
   // Redireciona console para o logger
-  console.log = (...args: unknown[]) => logger.log(util.format(...args))
-  console.info = (...args: unknown[]) => logger.info(util.format(...args))
-  console.warn = (...args: unknown[]) => logger.warn(util.format(...args))
-  console.error = (...args: unknown[]) => logger.error(util.format(...args))
-  console.debug = (...args: unknown[]) => logger.debug(util.format(...args))
+  console.log = (...args) => logger.info(...args)
+  console.info = (...args) => logger.info(...args)
+  console.warn = (...args) => logger.warn(...args)
+  console.error = (...args) => logger.error(...args)
+  console.debug = (...args) => logger.debug(...args)
 } else {
   // No client, logger é um objeto fake
   logger = {
