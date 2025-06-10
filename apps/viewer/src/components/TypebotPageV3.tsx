ef82ff81a65482e11d4875116ca47d435f64e7a1
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2025-06-10 12:46:16 +0200

    💚 Fix typecheck error in TypebotPageV3

--- apps/viewer/src/components/TypebotPageV3.tsx
+++ apps/viewer/src/components/TypebotPageV3.tsx
@@ -68,7 +68,7 @@ export const TypebotPageV3 = ({
         isMatchingViewerUrl={isMatchingViewerUrl}
       />
       <Standard
-        typebot={publicId}
+        typebot={publicId ?? undefined}
         onInit={clearQueryParamsIfNecessary}
         font={font ?? undefined}
         apiHost={apiOrigin}
