8e456012f2e4db52700d147015d880ed74f7dd06
Baptiste Arnaud <baptiste.arnaud95@gmail.com>
2025-06-18 13:03:45 +0200

    🔨 Add yesterday's total visitors in daily notification script

--- packages/scripts/src/playground.ts
+++ packages/scripts/src/playground.ts
@@ -1,41 +1,8 @@
-import prisma from "@typebot.io/prisma";
-import { promptAndSetEnvironment } from "./utils";
+import { getLandingPageVisitors } from "./helpers/getLandingPageVisitors";
 
 const executePlayground = async () => {
-  await promptAndSetEnvironment();
-
-  const result = await prisma.workspace.findMany({
-    where: {
-      members: {
-        some: {
-          user: {
-            email: "",
-          },
-        },
-      },
-    },
-    include: {
-      members: true,
-      typebots: {
-        select: {
-          name: true,
-          riskLevel: true,
-          id: true,
-        },
-      },
-    },
-  });
-  console.log(JSON.stringify(result));
-
-  // await prisma.bannedIp.deleteMany({})
-
-  // const result = await prisma.coupon.findMany({
-  //   where: {
-  //     code: '',
-  //   },
-  // })
-
-  // console.log(result)
+  const result = await getLandingPageVisitors();
+  console.log(result);
 };
 
 executePlayground();
