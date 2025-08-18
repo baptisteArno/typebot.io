1d6a500e5cfa41e474bddb69bb1ebe92e53116f9
Baptiste Arnaud <baptiste@typebot.io>
2025-08-18 12:45:09 +0200

    🐛 Only delete session with empty state if it's not in replying state

--- packages/chat-session/src/queries/removeIsReplyingInChatSession.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@typebot.io/prisma";
-
-export const removeIsReplyingInChatSession = async (id: string) =>
-  prisma.chatSession.updateMany({
-    where: { id },
-    data: { isReplying: false },
-  });
