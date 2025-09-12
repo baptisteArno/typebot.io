51a3c4558355cd7e3876a37683ea3d6f231c5413
fjunqueira <fabio.blumm18@gmail.com>
2025-09-12 11:39:53 -0300

    fix cookie settings in order to allow iframe requests

--- apps/builder/src/pages/api/auth/[...nextauth].ts
+++ apps/builder/src/pages/api/auth/[...nextauth].ts
@@ -237,6 +237,35 @@ export const getAuthOptions = ({
     newUser: env.NEXT_PUBLIC_ONBOARDING_TYPEBOT_ID ? '/onboarding' : undefined,
     error: '/signin',
   },
+  cookies: {
+    sessionToken: {
+      name: '__Secure-next-auth.session-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+    callbackUrl: {
+      name: '__Secure-next-auth.callback-url',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+    csrfToken: {
+      name: '__Host-next-auth.csrf-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'none',
+        path: '/',
+        secure: true,
+      },
+    },
+  },
   events: {
     signIn({ user }) {
       Sentry.setUser({ id: user.id })
