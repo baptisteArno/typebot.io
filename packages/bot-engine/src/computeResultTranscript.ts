d8bb1f7c4035a6126ae6e875802276c799957ea4
Baptiste Arnaud <baptiste@typebot.io>
2025-09-17 12:16:45 +0200

    🐛 Fix result transcript when several variables are set in 1 block

--- packages/bot-engine/src/computeResultTranscript.ts
+++ packages/bot-engine/src/computeResultTranscript.ts
@@ -176,7 +176,7 @@ const executeGroup = ({
     const typebot = typebotsQueue[0]?.typebot;
     if (!typebot) throw new Error("Typebot not found in session");
 
-    if (setVariableHistory.peek()?.blockId === block.id) {
+    while (setVariableHistory.peek()?.blockId === block.id) {
       typebot.variables = applySetVariable(setVariableHistory.next(), typebot);
     }
 
