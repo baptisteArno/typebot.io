eedf998420c6dab144b0b8dfd52b0ffd476a24bf
Baptiste Arnaud <baptiste@typebot.io>
2025-11-11 12:49:08 +0100

    🐛 Fix retrocompat on `parseColumnsOrder`

--- packages/results/src/parseColumnsOrder.ts
+++ packages/results/src/parseColumnsOrder.ts
@@ -3,4 +3,8 @@ import type { ResultHeaderCell } from "./schemas/results";
 export const parseColumnsOrder = (
   existingOrder: string[] | undefined,
   resultHeader: ResultHeaderCell[],
-) => ["select", ...(existingOrder ?? resultHeader.map((h) => h.id)), "logs"];
+) => {
+  return existingOrder?.at(0) === "select"
+    ? existingOrder
+    : ["select", ...(existingOrder ?? resultHeader.map((h) => h.id)), "logs"];
+};
