c18cc93aaba023d363e0224b22bdd5620a3a4343
MeBSilva <38053552+MeBSilva@users.noreply.github.com>
2026-03-03 14:29:01 -0300

    :construction: readded authentication via cognito based token (#167)
    * :construction: readded authentication via cognito based token
    
    * :construction: fixed query param
    
    * :construction: fixed logger import

--- apps/builder/src/features/auth/helpers/getAuthenticatedUser.ts
+++ apps/builder/src/features/auth/helpers/getAuthenticatedUser.ts
@@ -6,12 +6,18 @@ import { getServerSession } from 'next-auth'
 import { env } from '@typebot.io/env'
 import { mockedUser } from '@typebot.io/lib/mockedUser'
 import { DatabaseUserWithCognito } from '../types/cognito'
+import { verifyCognitoToken } from './verifyCognitoToken'
+import logger from '@/helpers/logger'
 
 export const getAuthenticatedUser = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<DatabaseUserWithCognito | undefined> => {
   const bearerToken = extractBearerToken(req)
+
+  if (req.query.apiGateway === 'true' && bearerToken)
+    return authenticateByEmbeddedToken(bearerToken)
+
   if (bearerToken) return authenticateByToken(bearerToken)
 
   const session = env.NEXT_PUBLIC_E2E_TEST
@@ -24,13 +30,43 @@ export const getAuthenticatedUser = async (
   return user
 }
 
+const authenticateByEmbeddedToken = async (
+  token: string
+): Promise<DatabaseUserWithCognito | undefined> => {
+  try {
+    const payload = await verifyCognitoToken({
+      cognitoAppClientId: env.CLOUDCHAT_COGNITO_APP_CLIENT_ID,
+      cognitoIssuerUrl: env.COGNITO_ISSUER_URL,
+      cognitoToken: token,
+    })
+
+    const user = await prisma.user.findUnique({
+      where: { email: payload.email },
+    })
+
+    if (!user) return
+
+    return {
+      ...user,
+      cognitoClaims: {
+        'custom:hub_role': payload['custom:hub_role'],
+        'custom:tenant_id': payload['custom:tenant_id'],
+        'custom:claudia_projects': payload['custom:claudia_projects'],
+      },
+    }
+  } catch (error) {
+    logger.error('Error in verify cognito token', { error })
+  }
+}
+
 const authenticateByToken = async (
   apiToken: string
 ): Promise<DatabaseUserWithCognito | undefined> => {
   if (typeof window !== 'undefined') return
   const user = (await prisma.user.findFirst({
     where: { apiTokens: { some: { token: apiToken } } },
-  })) as DatabaseUserWithCognito
+  })) as DatabaseUserWithCognito | null
+  if (!user) return
   Sentry.setUser({ id: user.id })
   return user
 }
