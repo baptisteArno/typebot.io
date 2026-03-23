9d9f02b3ac03604fea4caf1b9ab005870e2c8634
Baptiste Arnaud <baptiste@typebot.io>
2026-03-23 11:17:29 +0100

    🐛 Really restore ORPCPlugin on viewer

--- apps/viewer/src/app/api/[[...rest]]/route.ts
+++ apps/viewer/src/app/api/[[...rest]]/route.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHandler } from "@orpc/openapi/fetch";
 import { OpenAPIReferencePlugin } from "@orpc/openapi/plugins";
 import { onError } from "@orpc/server";
+import { CORSPlugin } from "@orpc/server/plugins";
 import { ZodToJsonSchemaConverter } from "@orpc/zod/zod4";
 import { authenticateWithBearerToken } from "@typebot.io/auth/helpers/authenticateWithBearerToken";
 import {
@@ -26,6 +27,7 @@ const handler = new OpenAPIHandler(appRouter, {
     }),
   ],
   plugins: [
+    new CORSPlugin(),
     new OpenAPIReferencePlugin({
       specPath: "/openapi.json",
       schemaConverters: [new ZodToJsonSchemaConverter()],
